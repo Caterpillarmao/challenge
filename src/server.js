1e39dd333702a188a3285d9009dd5fbad2ab46e8
mingjingmao <mao@mingjingmaosMBP.home>
2021-07-18 13:04:40 -0400

    remove cores

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const registerRoutes = require('./routes');
-const cors = require('cors');
+
 
 // server config
 const port = process.env.PORT || 3000;
@@ -10,10 +10,7 @@ const port = process.env.PORT || 3000;
 // register routes
 registerRoutes(app);
 app.use(express.static('public'));
-app.use(cors({
-    credentials: true, 
-    origin: 'http://localhost:3000'
-}));
+
 // create server start method
 const start = () => {
     return new Promise((resolve, reject) => {
